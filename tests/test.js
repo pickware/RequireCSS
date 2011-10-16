6c6a1dc62fb388a7d6d73ca4dbcb913d49bc03b7
Samuel Vogel <samuel.vogel@viison.com>
2011-10-16 19:01:01 +0200

    Refactor test to be jQuery free

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,16 @@
 var start = new Date().getTime()
 	log = function(message) {
-		$('#log').append('<div>' + (new Date().getTime() - start) + 'ms: ' + message + '</div>');
+		var log = document.getElementById('log'),
+			div = document.createElement('div');
+
+		div.innerHTML = (new Date().getTime() - start) + 'ms: ' + message;
+
+		log.appendChild(div);
+	},
+	success = function() {
+		var body = document.getElementsByTagName('body')[0];
+
+		body.style.backgroundColor = "green";
 	};
 
 require.config({
