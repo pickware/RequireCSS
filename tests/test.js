a9b40104c4f9cda7468ecaf5ad1fc456c822b5aa
Samuel Vogel <samuel.vogel@viison.com>
2011-10-16 17:15:48 +0200

    Format code in test.js

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 var start = new Date().getTime()
 	log = function(message) {
-	$('#log').append('<div>' + (new Date().getTime() - start) + 'ms: ' + message + '</div>');
-};
+		$('#log').append('<div>' + (new Date().getTime() - start) + 'ms: ' + message + '</div>');
+	};
 
 require.config({
 	paths: {
